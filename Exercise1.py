3a6c5c2241939ab435acb9b0c39ca9663cbb1527
Mishra-Ashutosh-08 <mmishra.ashutosh22@gmail.com>
2020-04-28 10:42:37 +0530

    Exercise 01

---
+++ Exercise1.py
@@ -0,0 +1,6 @@
+# Create a dictionary and take input from the user and return the meaning of the word from the dictionary.
+
+dict = {"Solitude":"A state of isolation" , "Elegance":"A quality of style and grace" , "Aurora":"The dawn in the early morning" , "Euphoria":"A feeling of intense excitement"}
+print("Enter the word ")
+word = input()
+print(dict[word])
